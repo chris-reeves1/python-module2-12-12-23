6eb1c42867da25d6141e3ba4590fa6cfac6ecf84
chris-reeves1 <dr_gonzo19@hotmail.com>
2023-10-17 14:30:57 +0000

    2

---
+++ rock_paper_scissor.py
@@ -0,0 +1,25 @@
+# Make a rock paper scissors app that I can play against the computer. 
+# The program should keep track of the number of rounds played, the number of rounds
+# won by the user, and the number of rounds won by the computer. 
+# After each round, the program should display the results 
+# and ask the user if they want to play another round. 
+# You should use functions, loops, conditionals, random number generation to achieve this.
+
+import random
+
+def playRound():
+    options = []
+    computeer_choice =
+    user_choice = input....
+
+    if..
+        elif...
+
+
+
+num_of_rounds
+user_wins
+computer_wins
+
+while play_again == y:
+
