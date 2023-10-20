a5e7e3d88ae8a3b0360813f6cd8ae35c1c649c1f
chris-reeves1 <dr_gonzo19@hotmail.com>
2023-10-20 09:21:38 +0000

    2

--- final-task.py
+++ final-task.py
@@ -3,7 +3,7 @@
 # 
 # Write a class that has a method that checks the password strength.
 # Use factors like length, upper/lower case and if it has a number and special character.
-# ratings hould be very weak - weak - moderate - strong - very strong 
+# ratings should be very weak - weak - moderate - strong - very strong 
 # Check against a list of common passwords 10-20 common password = very weak
 # User input that loops until the user quits
 # A dictionary that returns a history of passwords/strengths whilst in the loop.
